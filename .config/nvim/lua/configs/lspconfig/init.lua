ab34ae4710c3a64dfb174a6c18b7b51cea0269be
RyaWcksn <pram.aryawcksn@gmail.com>
2024-07-23 08:22:31 +0700

    update

--- .config/nvim/lua/configs/lspconfig/init.lua
+++ .config/nvim/lua/configs/lspconfig/init.lua
@@ -254,6 +254,16 @@ local on_attach = function(client, bufnr)
 		}
 	end
 end
+vim.lsp.util.apply_text_document_edit = function(text_document_edit, index, offset_encoding)
+	local text_document = text_document_edit.textDocument
+	local bufnr = vim.uri_to_bufnr(text_document.uri)
+	if offset_encoding == nil then
+		vim.notify_once('apply_text_document_edit must be called with valid offset encoding', vim.log.levels
+			.WARN)
+	end
+
+	vim.lsp.util.apply_text_edits(text_document_edit.edits, bufnr, offset_encoding)
+end
 
 local servers = {
 	gopls = require('configs.lspconfig.languages.gopls').gopls(capabilities, on_attach),
